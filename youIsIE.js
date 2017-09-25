0770cb27b436888f45d4ab768a46df9f692f5475
Ryan Green <hi@ryan.green>
2017-09-25 10:59:55 -0400

    here's Johnny!

---
+++ youIsIE.js
@@ -0,0 +1,26 @@
+function GetIEVersion() {
+    var sAgent = window.navigator.userAgent;
+    var Idx = sAgent.indexOf("MSIE");
+
+    // If IE, return version number.
+    if (Idx > 0)
+        return parseInt(sAgent.substring(Idx+ 5, sAgent.indexOf(".", Idx)));
+
+    // If IE 11 then look for Updated user agent string.
+    else if (!!navigator.userAgent.match(/Trident\/7\./)){
+        return 11;
+    }
+    else {
+        return 0; //It is not IE
+    }
+}
+
+$( document ).ready(function() {
+    if (GetIEVersion() > 0) {
+        // Append the version to the <head>
+        $("head").addClass("ie" + GetIEVersion());
+
+        // Append a generic ie class to the <head>
+        $("head").addClass("internetExplorer");
+    }
+});
